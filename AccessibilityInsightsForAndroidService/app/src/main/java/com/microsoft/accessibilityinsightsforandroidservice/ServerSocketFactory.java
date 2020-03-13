0cea2cf04908f95ca42c060766e453dfd779ef34
Wahaj Abid <waabid@microsoft.com>
2020-03-13 16:54:26 -0700

    fix: use get localhost instead of loopback address

--- AccessibilityInsightsForAndroidService/app/src/main/java/com/microsoft/accessibilityinsightsforandroidservice/ServerSocketFactory.java
+++ AccessibilityInsightsForAndroidService/app/src/main/java/com/microsoft/accessibilityinsightsforandroidservice/ServerSocketFactory.java
@@ -10,6 +10,6 @@ import java.net.ServerSocket;
 public class ServerSocketFactory {
   public ServerSocket createServerSocket(int serverPort) throws IOException {
     // Create a localhost-only server socket
-    return new ServerSocket(serverPort, 0, InetAddress.getLoopbackAddress());
+    return new ServerSocket(serverPort, 0, InetAddress.getLocalHost());
   }
 }
