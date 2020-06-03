5f77b3c2bcfe29bcd23153b21414158dc40a5861
Dan Bjorge <dan@dbjorge.net>
2020-06-02 17:45:57 -0700

    Initial impl of fix for extra right-padding on screenshots

---
+++ AccessibilityInsightsForAndroidService/app/src/main/java/com/microsoft/accessibilityinsightsforandroidservice/ImageFormatException.java
@@ -0,0 +1,7 @@
+package com.microsoft.accessibilityinsightsforandroidservice;
+
+class ImageFormatException extends Exception {
+  public ImageFormatException(String message) {
+    super(message);
+  }
+}
--- AccessibilityInsightsForAndroidService/app/src/main/java/com/microsoft/accessibilityinsightsforandroidservice/OnScreenshotAvailable.java
+++ AccessibilityInsightsForAndroidService/app/src/main/java/com/microsoft/accessibilityinsightsforandroidservice/OnScreenshotAvailable.java
@@ -6,11 +6,17 @@ package com.microsoft.accessibilityinsightsforandroidservice;
 import android.graphics.Bitmap;
 import android.media.Image;
 import android.media.ImageReader;
+import android.nfc.FormatException;
 import android.util.DisplayMetrics;
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
 import java.util.function.Consumer;
 
 public class OnScreenshotAvailable implements ImageReader.OnImageAvailableListener {
+  private final Bitmap.Config IMAGE_BITMAP_FORMAT = Bitmap.Config.ARGB_8888;
+  private final int IMAGE_PIXEL_STRIDE = 4; // Implied by ARGB_8888 (4 bytes per pixel)
+
   private static final String TAG = "OnScreenshotAvailable";
   private boolean imageAlreadyProcessed = false;
   private Consumer<Bitmap> bitmapConsumer;
@@ -31,26 +37,56 @@ public class OnScreenshotAvailable implements ImageReader.OnImageAvailableListen
     }
 
     Image image = imageReader.acquireLatestImage();
-    Bitmap screenshotBitmap = getBitmapFromImage(image);
-    image.close();
-    imageAlreadyProcessed = true;
-    bitmapConsumer.accept(screenshotBitmap);
+    try {
+      Bitmap screenshotBitmap = getBitmapFromImage(image);
+      image.close();
+      imageAlreadyProcessed = true;
+      bitmapConsumer.accept(screenshotBitmap);
+    } catch(ImageFormatException e) {
+      Logger.logError(TAG, "ImageFormatException: " + e.toString());
+      image.close();
+    }
   }
 
-  private Bitmap getBitmapFromImage(Image image) {
-    Image.Plane[] imagePlanes = image.getPlanes();
-    int bitmapWidth = getBitmapWidth(image, imagePlanes);
-    Bitmap screenshotBitmap =
-        bitmapProvider.createBitmap(bitmapWidth, metrics.heightPixels, Bitmap.Config.ARGB_8888);
-    ByteBuffer buffer = imagePlanes[0].getBuffer();
-    screenshotBitmap.copyPixelsFromBuffer(buffer);
-    return screenshotBitmap;
+  private Bitmap getBitmapFromImage(Image image) throws ImageFormatException {
+    int width = image.getWidth();
+    int height = image.getHeight();
+    if (width != metrics.widthPixels || height != metrics.heightPixels) {
+      Logger.logError(TAG, "Received image of dimensions " + width + "x" + height + ", mismatches device DisplayMetrics " + metrics.widthPixels + "x" + metrics.heightPixels);
+    }
+
+    Bitmap bitmap = bitmapProvider.createBitmap(width, height, IMAGE_BITMAP_FORMAT);
+    copyPixelsFromImagePlane(bitmap, image.getPlanes()[0], width, height);
+
+    return bitmap;
   }
 
-  private int getBitmapWidth(Image image, Image.Plane[] imagePlanes) {
-    int pixelStride = imagePlanes[0].getPixelStride();
-    int rowStride = imagePlanes[0].getRowStride();
-    int rowPadding = rowStride - pixelStride * metrics.widthPixels;
-    return image.getWidth() + rowPadding / pixelStride;
+  private void copyPixelsFromImagePlane(Bitmap destination, Image.Plane source, int width, int height) throws ImageFormatException {
+    // Note: rowStride is usually pixelStride * width, but can sometimes be larger (with padding)
+    int pixelStride = source.getPixelStride(); // bytes per pixel
+    int rowStride = source.getRowStride(); // bytes per row
+
+    if (pixelStride != IMAGE_PIXEL_STRIDE) {
+      throw new ImageFormatException("Invalid source Image: pixelStride=" + pixelStride + ", expected " + IMAGE_PIXEL_STRIDE);
+    }
+    if (rowStride < width * pixelStride) {
+      throw new ImageFormatException("Invalid source Image: rowStride " + rowStride + " is too small for width " + width + " at pixelStride " + pixelStride);
+    }
+
+    ByteBuffer sourceBuffer = source.getBuffer();
+
+    if (rowStride == width * pixelStride) {
+      destination.copyPixelsFromBuffer(sourceBuffer);
+      return;
+    }
+
+    int unpaddedRowStride = width * pixelStride;
+    ByteBuffer pixelDataWithRowPaddingRemoved = ByteBuffer.allocate(unpaddedRowStride * height);
+    for (int row = 0; row < height; ++row) {
+      sourceBuffer.position(row * rowStride);
+      sourceBuffer.get(pixelDataWithRowPaddingRemoved.array(), row * unpaddedRowStride, unpaddedRowStride);
+    }
+
+    destination.copyPixelsFromBuffer(pixelDataWithRowPaddingRemoved);
   }
 }
