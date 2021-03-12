75b74fa60252884f56265689cc81a4ec0e00fe9f
Wahaj Abid <waabid@microsoft.com>
2021-03-12 11:15:37 -0800

    lint fixes

--- AccessibilityInsightsForAndroidService/app/src/main/java/com/microsoft/accessibilityinsightsforandroidservice/FocusVisualizationStateManager.java
+++ AccessibilityInsightsForAndroidService/app/src/main/java/com/microsoft/accessibilityinsightsforandroidservice/FocusVisualizationStateManager.java
@@ -1,36 +1,40 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
 package com.microsoft.accessibilityinsightsforandroidservice;
 
 import java.util.ArrayList;
 import java.util.function.Consumer;
 
 public class FocusVisualizationStateManager {
-    private boolean enabled = false;
-    private ArrayList<Consumer<Boolean>> onChangedListeners;
+  private boolean enabled = false;
+  private ArrayList<Consumer<Boolean>> onChangedListeners;
 
-    public FocusVisualizationStateManager() {
-        onChangedListeners = new ArrayList<Consumer<Boolean>>();
+  public FocusVisualizationStateManager() {
+    onChangedListeners = new ArrayList<Consumer<Boolean>>();
+  }
+
+  public void subscribe(Consumer<Boolean> listener) {
+    onChangedListeners.add(listener);
+  }
+
+  public void setState(boolean enabled) {
+    if (this.enabled == enabled) {
+      return;
     }
 
-    public void subscribe(Consumer<Boolean> listener) {
-        onChangedListeners.add(listener);
-    }
+    this.enabled = enabled;
+    this.emitChanged(enabled);
+  }
 
-    public void setState(boolean enabled) {
-        if (this.enabled == enabled) {
-            return;
-        }
+  public boolean getState() {
+    return this.enabled;
+  }
 
-        this.enabled = enabled;
-        this.emitChanged(enabled);
-    }
-
-    public boolean getState() {
-        return this.enabled;
-    }
-
-    private void emitChanged(boolean enabled) {
-        onChangedListeners.forEach(listener -> {
-            listener.accept(enabled);
+  private void emitChanged(boolean enabled) {
+    onChangedListeners.forEach(
+        listener -> {
+          listener.accept(enabled);
         });
-    }
+  }
 }
