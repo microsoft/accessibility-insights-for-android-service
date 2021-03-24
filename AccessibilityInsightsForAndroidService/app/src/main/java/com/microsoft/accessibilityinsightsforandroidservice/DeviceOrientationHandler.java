e3ffff24b826fe48b85b272a6dbdfaf3aa0fe0cb
Brock Taylor <13286385+brocktaylor7@users.noreply.github.com>
2021-03-24 10:42:44 -0700

    feat(focus-mvp-android-device): Device Orientation Handler (#84)
    * Device Orientation Handler
    
    * only check orientation from within handler
    
    * removed getOrientation, updated tests

---
+++ AccessibilityInsightsForAndroidService/app/src/main/java/com/microsoft/accessibilityinsightsforandroidservice/DeviceOrientationHandler.java
@@ -0,0 +1,37 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+package com.microsoft.accessibilityinsightsforandroidservice;
+
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+public class DeviceOrientationHandler {
+  private int orientation;
+  private ArrayList<Consumer<Integer>> onOrientationChangedListeners;
+
+  public DeviceOrientationHandler(int orientation) {
+    this.orientation = orientation;
+    this.onOrientationChangedListeners = new ArrayList<>();
+  }
+
+  public void subscribe(Consumer<Integer> listener) {
+    this.onOrientationChangedListeners.add(listener);
+  }
+
+  public void setOrientation(int orientation) {
+    if (this.orientation == orientation) {
+      return;
+    }
+
+    this.orientation = orientation;
+    this.emitChanged(orientation);
+  }
+
+  private void emitChanged(int orientation) {
+    this.onOrientationChangedListeners.forEach(
+        listener -> {
+          listener.accept(orientation);
+        });
+  }
+}
