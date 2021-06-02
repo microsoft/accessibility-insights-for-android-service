52d3c1e8107c12522133a95ca260c33ee0841bde
Lisa Li <lisli@microsoft.com>
2021-06-02 14:47:06 -0700

    add ATFAScannerFactory and test

---
+++ AccessibilityInsightsForAndroidService/app/src/test/java/com/microsoft/accessibilityinsightsforandroidservice/ATFAScannerFactoryTest.java
@@ -0,0 +1,27 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+package com.microsoft.accessibilityinsightsforandroidservice;
+
+import android.content.Context;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ATFAScannerFactoryTest {
+  @Mock Context contextMock;
+
+  @Before
+  public void prepare() {}
+
+  @Test
+  public void atfaScannerExists() {
+    Assert.assertNotNull(
+        ATFAScannerFactory.createATFAScanner(contextMock));
+  }
+}
