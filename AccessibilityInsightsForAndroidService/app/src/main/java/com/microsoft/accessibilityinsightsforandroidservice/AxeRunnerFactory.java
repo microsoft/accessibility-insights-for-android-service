0491f83f74ad4d07b204ad087c34d170ecc1ee52
Dan Bjorge <danielbj@microsoft.com>
2021-07-27 15:24:32 -0400

    Suppress an axe-android deprecation warning with no workaround

--- AccessibilityInsightsForAndroidService/app/src/main/java/com/microsoft/accessibilityinsightsforandroidservice/AxeRunnerFactory.java
+++ AccessibilityInsightsForAndroidService/app/src/main/java/com/microsoft/accessibilityinsightsforandroidservice/AxeRunnerFactory.java
@@ -8,8 +8,15 @@ import com.deque.axe.android.AxeConf;
 import com.deque.axe.android.constants.AxeStandard;
 
 public class AxeRunnerFactory {
+  // AxeConf.removeStandard is marked deprecated, but is still suggested in the axe-android docs as
+  // the recommended way to constrain which standards we scan against.
+  //
+  // https://github.com/dequelabs/axe-android/issues/145 tracks the request for a non-deprecated
+  // replacement option.
+  @SuppressWarnings("deprecation")
   public Axe createAxeRunner() {
     AxeConf axeConf = new AxeConf();
+
     axeConf.removeStandard(AxeStandard.BEST_PRACTICE);
     axeConf.removeStandard(AxeStandard.PLATFORM);
     return new Axe(axeConf);
