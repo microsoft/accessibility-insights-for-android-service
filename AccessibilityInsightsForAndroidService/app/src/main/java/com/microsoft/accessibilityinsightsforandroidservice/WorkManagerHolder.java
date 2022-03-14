d2592f8c3ef476484d3f9cfba330aa1d5b90d3fe
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2022-03-14 15:10:22 -0700

    Run gradlew spotlessApply

--- AccessibilityInsightsForAndroidService/app/src/main/java/com/microsoft/accessibilityinsightsforandroidservice/WorkManagerHolder.java
+++ AccessibilityInsightsForAndroidService/app/src/main/java/com/microsoft/accessibilityinsightsforandroidservice/WorkManagerHolder.java
@@ -4,23 +4,22 @@
 package com.microsoft.accessibilityinsightsforandroidservice;
 
 import android.content.Context;
-
 import androidx.work.Configuration;
 import androidx.work.WorkManager;
-
 import java.util.HashMap;
 
 public class WorkManagerHolder {
-    private static HashMap<Context, WorkManager> ContextToManagerMap = new HashMap<Context, WorkManager>();
+  private static HashMap<Context, WorkManager> ContextToManagerMap =
+      new HashMap<Context, WorkManager>();
 
-    public static WorkManager getWorkManager(Context context) {
-        WorkManager managerForContext = ContextToManagerMap.get(context);
+  public static WorkManager getWorkManager(Context context) {
+    WorkManager managerForContext = ContextToManagerMap.get(context);
 
-        if (managerForContext == null) {
-            WorkManager.initialize(context, new Configuration.Builder().build());
-            managerForContext = WorkManager.getInstance(context);
-            ContextToManagerMap.put(context, managerForContext);
-        }
-        return managerForContext;
+    if (managerForContext == null) {
+      WorkManager.initialize(context, new Configuration.Builder().build());
+      managerForContext = WorkManager.getInstance(context);
+      ContextToManagerMap.put(context, managerForContext);
     }
+    return managerForContext;
+  }
 }
