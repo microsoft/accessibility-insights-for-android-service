c64d309a8ae20701d132b3b09eefa3872ab10bcc
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2022-03-15 10:39:32 -0700

    Add a lock to avoid race conditions during initialization

--- AccessibilityInsightsForAndroidService/app/src/main/java/com/microsoft/accessibilityinsightsforandroidservice/WorkManagerHolder.java
+++ AccessibilityInsightsForAndroidService/app/src/main/java/com/microsoft/accessibilityinsightsforandroidservice/WorkManagerHolder.java
@@ -9,16 +9,19 @@ import androidx.work.WorkManager;
 import java.util.WeakHashMap;
 
 public class WorkManagerHolder {
+  private static final Object LockObject = new Object();
   private static WeakHashMap<Context, WorkManager> ContextToManagerMap =
       new WeakHashMap<Context, WorkManager>();
 
   public static WorkManager getWorkManager(Context context) {
-    WorkManager managerForContext = ContextToManagerMap.get(context);
+    synchronized (LockObject) {
+      WorkManager managerForContext = ContextToManagerMap.get(context);
 
-    if (managerForContext == null) {
-      WorkManager.initialize(context, new Configuration.Builder().build());
-      managerForContext = WorkManager.getInstance(context);
-      ContextToManagerMap.put(context, managerForContext);
+      if (managerForContext == null) {
+        WorkManager.initialize(context, new Configuration.Builder().build());
+        managerForContext = WorkManager.getInstance(context);
+        ContextToManagerMap.put(context, managerForContext);
+      }
     }
     return managerForContext;
   }
