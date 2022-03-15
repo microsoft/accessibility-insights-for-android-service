f888fdca687d5eb35985e98a5b1c4ba01de098d1
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2022-03-15 11:26:43 -0700

    chore: Improve perf and reliability of WorkManagerHolder (#153)

--- AccessibilityInsightsForAndroidService/app/src/main/java/com/microsoft/accessibilityinsightsforandroidservice/WorkManagerHolder.java
+++ AccessibilityInsightsForAndroidService/app/src/main/java/com/microsoft/accessibilityinsightsforandroidservice/WorkManagerHolder.java
@@ -6,20 +6,23 @@ package com.microsoft.accessibilityinsightsforandroidservice;
 import android.content.Context;
 import androidx.work.Configuration;
 import androidx.work.WorkManager;
-import java.util.HashMap;
+import java.util.WeakHashMap;
 
 public class WorkManagerHolder {
-  private static HashMap<Context, WorkManager> ContextToManagerMap =
-      new HashMap<Context, WorkManager>();
+  private static final Object LockObject = new Object();
+  private static WeakHashMap<Context, WorkManager> ContextToManagerMap =
+      new WeakHashMap<Context, WorkManager>();
 
   public static WorkManager getWorkManager(Context context) {
-    WorkManager managerForContext = ContextToManagerMap.get(context);
+    synchronized (LockObject) {
+      WorkManager managerForContext = ContextToManagerMap.get(context);
 
-    if (managerForContext == null) {
-      WorkManager.initialize(context, new Configuration.Builder().build());
-      managerForContext = WorkManager.getInstance(context);
-      ContextToManagerMap.put(context, managerForContext);
+      if (managerForContext == null) {
+        WorkManager.initialize(context, new Configuration.Builder().build());
+        managerForContext = WorkManager.getInstance(context);
+        ContextToManagerMap.put(context, managerForContext);
+      }
+      return managerForContext;
     }
-    return managerForContext;
   }
 }
