5e98256e4fc19afaa45116ccf553d3fe74279513
John Alkire <john.alkire@microsoft.com>
2022-04-22 09:04:13 -0700

    Remove duplicate task + reduce waiting

--- pipeline/scripts/setup-emulator.sh
+++ pipeline/scripts/setup-emulator.sh
@@ -14,12 +14,12 @@ echo "Starting emulator"
 # Start emulator in background
 nohup $ANDROID_HOME/emulator/emulator -avd "$emulator_name" -no-snapshot -no-boot-anim > /dev/null 2>&1 &
 $ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
+sleep 30 # pause to wait for the emulator to finish booting
 
 $ANDROID_HOME/platform-tools/adb devices
 echo "Emulator started"
 
 $ANDROID_HOME/platform-tools/adb install app-debug.apk
 $ANDROID_HOME/platform-tools/adb shell appops set com.microsoft.accessibilityinsightsforandroidservice PROJECT_MEDIA allow
-sleep 30
 $ANDROID_HOME/platform-tools/adb shell settings put secure enabled_accessibility_services com.microsoft.accessibilityinsightsforandroidservice/com.microsoft.accessibilityinsightsforandroidservice.AccessibilityInsightsForAndroidService
-sleep 30
+sleep 10 # pause to let the accessibility service start
