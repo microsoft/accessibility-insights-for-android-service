f66302bc30aac96fd098f21f8ef824693812a38a
John Alkire <4615491+jalkire@users.noreply.github.com>
2022-05-09 09:24:13 -0700

    chore: Initial pass at adding e2e tests for Android service (#156)
    #### Details
    This PR contains some initial work to add "e2e tests" for the Android service. The e2e tests consist of some bash scripts that start up an emulator for various API levels and on each one install the service, make config and result calls to the service, and fail if stderr is not empty. This is meant as a first step--in the future, we should perform more validation.
    
    ##### Motivation
    We've had a few issues that rendered this service unusable only on particular API levels. This PR will help us avoid similar issues in the future without requiring additional manual testing.
    
    ##### Contexta
    Some open questions:
    - The bash script in here is based on the [ADO docs for the Android Emulator](https://docs.microsoft.com/en-us/azure/devops/pipelines/ecosystems/android?view=azure-devops#test-on-the-android-emulator). Should we follow this approach or switch it over to be a JS script like the verification scripts already in this repo?
    - The "verification" part of this draft just runs some ADB commands directly and verifies stderr is empty. This should ultimately be improved upon. Is the current state a good enough first step, or do we want to actually validate the responses before merging anything?
    - If we want to validate, either as a part of this PR or in the future, should that be done in a script (either bash or JS), or in a [more traditional Android test](https://developer.android.com/training/testing/instrumented-tests)?
    - Should these run as a part of PRs, CI, and/or release? For reference, they add ~6-7 minutes to the current PR/CI pipeline time.
    - How many and which APIs should we run these against? I picked 24 (our [minimum supported](https://accessibilityinsights.io/docs/android/reference/faq/#what-are-the-android-apis-supported-by-the-accessibility-insights-for-android-service)), 28 (our targetSdk), 30 (the [most popular](https://apilevels.com/)), and 32 (the latest).
    
    Please feel free to provide any thoughts to these questions or point out new ones!
    
    #### Pull request checklist
    <!-- If a checklist item is not applicable to this change, write "n/a" in the checkbox -->
    
    - [n/a] Addresses an existing issue: #0000
    - [n/a] Added/updated relevant unit test(s)
    - [n/a] Ran `./gradlew fastpass` from `AccessibilityInsightsForAndroidService`
    - [x] PR title _AND_ final merge commit title both start with a semantic tag (`fix:`, `chore:`, `feat(feature-name):`, `refactor:`).

---
+++ pipeline/scripts/setup-emulator.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+emulator_config="system-images;android-$ANDROID_API_LEVEL;google_apis;x86_64"
+emulator_name="emulator_api_$ANDROID_API_LEVEL"
+
+# Install AVD files
+echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install "$emulator_config"
+# Create emulator
+echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n "$emulator_name" -k "$emulator_config" --force
+
+$ANDROID_HOME/emulator/emulator -list-avds
+
+echo "Starting emulator"
+# Start emulator in background
+nohup $ANDROID_HOME/emulator/emulator -avd "$emulator_name" -no-snapshot -no-boot-anim > /dev/null 2>&1 &
+
+echo "Waiting for boot_completed to have value"
+$ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do echo -n "."; sleep 1; done; input keyevent 82; echo "."'
+
+echo "Pausing to allow emulator to finish booting"
+sleep 20 # pause to wait for the emulator to finish booting
+
+$ANDROID_HOME/platform-tools/adb devices
+echo "Emulator started"
+
+echo "Installing and enabling Accessibility Insights for Android Service"
+$ANDROID_HOME/platform-tools/adb install app-debug.apk
+$ANDROID_HOME/platform-tools/adb shell appops set com.microsoft.accessibilityinsightsforandroidservice PROJECT_MEDIA allow
+$ANDROID_HOME/platform-tools/adb shell settings put secure enabled_accessibility_services com.microsoft.accessibilityinsightsforandroidservice/com.microsoft.accessibilityinsightsforandroidservice.AccessibilityInsightsForAndroidService
+
+echo "Pausing to allow service to start"
+sleep 5 # pause to let the accessibility service start
